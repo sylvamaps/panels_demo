bd6216f3aa765002f079274f713247e462d6aaa0
sylvamaps <95078720+sylvamaps@users.noreply.github.com>
2021-12-01 16:21:22 +0100

    Add files via upload

---
+++ app/app.R
@@ -0,0 +1,59 @@
+library(shinypanels)
+
+ui <- panelsPage(
+    panel(title = "Panel Hidden",
+          width = 300,
+          hidden = TRUE,
+          body = h1("Nothing here")
+    ),
+    panel(
+        title = "Panel 1",
+        width = 300,
+        color = "magenta",
+        hidden = FALSE,
+        body = div(
+            h3("Here is some info"),
+            tags$hr(),
+            verbatimTextOutput("debug"),
+            tags$hr(),
+            p("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+        ),
+        tags$hr(),
+        h2("More info")
+    ),
+    panel(
+        title = "Panel 3",
+        width = 350,
+        collapsed = TRUE,
+        body = div(
+            plotOutput("plot")
+        )
+    ),
+    panel(
+        title = "Panel 4",
+        can_collapse = FALSE,
+        body = div(
+        ),
+        footer = HTML("Footer")
+    )
+)
+
+
+
+server <-  function(input, output, session) {
+    
+    output$debug <- renderPrint({
+        cat(paste(c("La", "casa", "en", "el", "árbol"), collapse = "\n"))
+    })
+    output$plot <- renderPlot(
+        plot(cars)
+    )
+    
+}
+
+shinyApp(ui, server)
